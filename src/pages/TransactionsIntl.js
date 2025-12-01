ec1a22e13d6cc63a4bb6575456cb548efc76769f
Rodrigo <r.pizarro.leeson@gmail.com>
2025-12-01 13:46:51 -0300

    fix: increase snackbar duration and position at top center

--- src/pages/TransactionsIntl.js
+++ src/pages/TransactionsIntl.js
@@ -431,7 +431,12 @@ const TransactionsIntl = () => {
         </DialogActions>
       </Dialog>
 
-      <Snackbar open={snackbar.open} autoHideDuration={3000} onClose={()=>setSnackbar(prev=>({ ...prev, open:false }))}>
+      <Snackbar 
+        open={snackbar.open} 
+        autoHideDuration={6000} 
+        onClose={()=>setSnackbar(prev=>({ ...prev, open:false }))}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
         <Alert onClose={()=>setSnackbar(prev=>({ ...prev, open:false }))} severity={snackbar.severity} sx={{ width: '100%' }}>
           {snackbar.message}
         </Alert>
